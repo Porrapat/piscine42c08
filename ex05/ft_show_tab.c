bbfeee036a9e000017d5ddda14a683d270b9f022
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-11-08 23:59:19 +0700

    Add More Good Code ex05

--- ex05/ft_show_tab.c
+++ ex05/ft_show_tab.c
@@ -11,78 +11,55 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
-
 #include "ft_stock_str.h"
 
-char	*ft_strdup(char *src)
+void	ft_putstring(char *str)
 {
-	int		index;
-	char	*dest;
+	int	i;
 
-	index = 0;
-	dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1);
-	if (dest == NULL)
-		return (0);
-	while (src[index])
+	i = 0;
+	while (str[i] != '\0')
 	{
-		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
-	return (dest);
-}
-
-void	ft_put_string(char *str)
-{
-	while (*str)
-	{
-		write(1, str, 1);
-		str++;
+		write(1, str + i, 1);
+		i++;
 	}
 }
 
-void	ft_put_number(int number)
+void	ft_putnbr(int nb)
 {
-	if (number > 9)
-		ft_put_number(number / 10);
-	write(1, &"0123456789"[number % 10], 1);
+	char	digit;
+
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	digit = nb % 10 + '0';
+	write(1, &digit, 1);
 }
 
 void	ft_show_tab(struct s_stock_str *par)
 {
-	int	index;
+	int	i;
 
-	index = 0;
-	while (par[index].str != 0)
+	i = 0;
+	while (par[i].str != 0)
 	{
-		ft_put_string(par[index].str);
-		ft_put_string("\n");
-		ft_put_number(par[index].size);
-		ft_put_string("\n");
-		ft_put_string(par[index].copy);
-		ft_put_string("\n");
-		index++;
+		ft_putstring(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstring(par[i].copy);
+		write(1, "\n", 1);
+		i++;
 	}
-}
-
-struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
-{
-	int					index;
-	struct s_stock_str	*array;
-
-	ac = ac + 0;
-	array = malloc((ac + 1) * sizeof(struct s_stock_str));
-	if (!array)
-		return (NULL);
-	index = 0;
-	while (index < ac)
-	{
-		array[index].size = ft_str_length(av[index]);
-		array[index].str = av[index];
-		array[index].copy = ft_strdup(av[index]);
-		index++;
-	}
-	array[index] = (struct s_stock_str){0, 0, 0};
-	return (array);
 }
